e7cca663cebb51b320495ee185de0eb34808a78f
Stagsted <rasmuskarnoestagsted@gmail.com>
2024-05-28 17:40:50 +0200

    Cleaned up the BletRouter class

--- BeltRouter.py
+++ BeltRouter.py
@@ -5,8 +5,13 @@ from utils import *
 class BeltRouter:
 
     def __init__(self, buildings, x, y, input_count, output_count, product_count, selector_belt_indicies, belt_length):
+        self.generate_splitters(buildings, x, y, input_count, output_count, product_count)
+        self.generate_input_belts(buildings, x, y, input_count, belt_length)
+        self.generate_output_and_product_belts(buildings, x, y, input_count, output_count, product_count, belt_length)
+        self.generate_selector_belts(buildings, x, y, input_count, output_count, product_count, selector_belt_indicies)
+        self.generate_production_belts(buildings, x, y, input_count, output_count, product_count)
         
-        ## Generate Splitters
+    def generate_splitters(self, buildings, x, y, input_count, output_count, product_count):
         self.input_splitters = []
         for i in range(input_count):
             buildings.append(
@@ -27,7 +32,7 @@ class BeltRouter:
             self.product_splitters.append(buildings[-1])
         self.splitters = self.input_splitters + self.output_splitters + self.product_splitters
         
-        ## Generate input belts
+    def generate_input_belts(self, buildings, x, y, input_count, belt_length):
         self.input_belts = []
         for i in range(input_count):
             belts = generate_belt(buildings, x + 2 * i, y + belt_length - 1, 1, Yaw.South, belt_length)
@@ -39,7 +44,7 @@ class BeltRouter:
             belt_end.connect_to_splitter(splitter)
             
             
-        ## Generate output- and product belts
+    def generate_output_and_product_belts(self, buildings, x, y, input_count, output_count, product_count, belt_length):
         self.output_belts = []
         for i in range(output_count):
             belts = generate_belt(buildings, x + 2 * (i + input_count), y, 1, Yaw.North, belt_length)
@@ -58,7 +63,7 @@ class BeltRouter:
             splitter = self.product_splitters[i]
             splitter.connect_to_belt(belt_start)
             
-        ## Generate selector belts
+    def generate_selector_belts(self, buildings, x, y, input_count, output_count, product_count, selector_belt_indicies):
         self.selector_belts = []
         for i in range(len(selector_belt_indicies)):
             ### Generate north bound belts
@@ -76,7 +81,8 @@ class BeltRouter:
             # TODO remove function "connect_cornor_belt" and implement it in Belt.connect_to_belt
             connect_cornor_belt(buildings, north_belt_end, east_belt_start)
         
-        ## Generate production belts
+    def generate_production_belts(self, buildings, x, y, input_count, output_count, product_count):
+
         self.production_belts = []
         for i in range(product_count):
             ### Generate west bounding belt
